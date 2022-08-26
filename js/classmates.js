dd76c72818c621a01f9f76a1e6920943be9499b3
Anyer <90461487+Anyerxkp@users.noreply.github.com>
2022-08-26 09:54:15 +0800

    Update classmates.js

--- js/classmates.js
+++ js/classmates.js
@@ -1,6 +1,6 @@
 	var names=["Lenard","Chen Jian","Wu Fan","Tai","Mia","Sabrina","Queenie","Xiao Min","Kok Wee","Chi Keong"];
 	var dis_names=document.getElementsByClassName('names')[0];
 	var names_list="";
-	for(var i=0;i<39;i++) names_list+="<li>"+names[i]+"</li>";
+	for(var i=0;i<11;i++) names_list+="<li>"+names[i]+"</li>";
 	dis_names.innerHTML=names_list;
 	console.log(names[0]);
